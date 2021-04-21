8f4355c35caa2e33d7983c1593260980f5cf3c95
EHOTIK911 <42716046+EHOTIK911@users.noreply.github.com>
2021-04-21 19:16:20 +0300

    Update 22.py

--- Additional assignments/var. 25000005/22.py
+++ Additional assignments/var. 25000005/22.py
@@ -1,14 +1,17 @@
 # test
-x = 0
+
 for i in range(1, 1000):
-    i = x
+    x = 0
+    z = 0
+    i = z
     a = 0
     b = 1
-    while x > 0:
-        if x % 2 > 0:
-            a += x % 8
-        else:
-            b *= x % 8
-        x = x // 8
-    if a == 2 and b == -24:
-        print(i)
+    if i == x:
+        while x > 0:
+            if x % 2 > 0:
+                a += x % 8
+            else:
+                b *= x % 8
+            x = x // 8
+            if a == 2 and b == -24:
+                print(z)
