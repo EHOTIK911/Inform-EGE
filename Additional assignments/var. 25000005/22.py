f72155bbbf2f531838759972b2eb6ca251c9e511
EHOTIK911 <42716046+EHOTIK911@users.noreply.github.com>
2021-04-21 18:23:10 +0300

    test

--- Additional assignments/var. 25000005/22.py
+++ Additional assignments/var. 25000005/22.py
@@ -1,5 +1,7 @@
 #test
 for x in range(1,1000):
+    i = 0
+    x = i
     a = 0
     b = 1
     while x > 0:
@@ -8,5 +10,5 @@ for x in range(1,1000):
         else:
             b *= x%8
         x = x//8
-    if a == 2 and b == 24:
-        print(x)
+    if a == 2 and b == -24:
+        print(i)
